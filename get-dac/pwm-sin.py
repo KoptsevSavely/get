e3cd9d7e12a2b84b5f905dd8ff0a867cef7da08d
Koptsev Savely <koptsev.sa@phystech.edu>
2025-10-15 18:27:58 +0300

    Добавлен коммит для синуса на шиме

---
+++ get-dac/pwm-sin.py
@@ -0,0 +1,25 @@
+import pwm_dac
+import signal_generator
+import time
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+
+
+signal_frequency = 10
+sampling_frequency = 1000
+pwm_pin = 12
+dynamic_range = 3
+
+
+
+if __name__ == "__main__":
+    try:
+        dac = pwm_dac.PWM_DAC(pwm_pin, sampling_frequency, dynamic_range)
+        t0 = time.time()
+        while True:
+            try:
+                dac.set_voltage(signal_generator.get_sin_wave_amplitude(signal_frequency ,time.time())*dynamic_range)
+            except ValueError:
+                print(ValueError)
+    finally:
+        dac.deinit()
